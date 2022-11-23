4aaad93b340e62d2af29a84b12f7d2d6bfe636e3
Juan Cruz Bafico <86856551+jBafico@users.noreply.github.com>
2022-11-23 16:47:18 -0300

    DEV: Camera takes photos :D

--- lib/Screens/scanner/scanner.dart
+++ lib/Screens/scanner/scanner.dart
@@ -1,6 +1,7 @@
+
+
 import 'package:camera/camera.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:flutter/material.dart';
 
 class ScannerScreen extends StatefulWidget {
   const ScannerScreen({super.key});
@@ -46,20 +47,78 @@ class _ScannerScreenState extends State<ScannerScreen> {
     });
   }
 
+
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
 
+  Future<XFile?> takePicture() async {
+    final CameraController cameraController = controller;
+    if (cameraController.value.isTakingPicture) {
+      // A capture is already pending, do nothing.
+      return null;
+    }
+    try {
+      XFile file = await cameraController.takePicture();
+      return file;
+    } on CameraException catch (e) {
+      return null;
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     if(!controllerInitialized || !controller.value.isInitialized){
       return Container();
     }
-    return AspectRatio(
-      aspectRatio: controller.value.aspectRatio,
-      child: CameraPreview(controller),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        AspectRatio(
+            aspectRatio: 1/controller.value.aspectRatio,
+            child: Stack(
+              children: <Widget>[
+            controller.buildPreview(),
+            Container(
+              alignment: AlignmentDirectional.bottomCenter,
+                child:
+                  InkWell(
+                    onTap: () async {
+                    XFile? rawImage = await takePicture();
+                    /*TODO QUE HACER CON LA RAW IMAGE
+                    File imageFile = File(rawImage.path);
+
+                    int currentUnix = DateTime.now().millisecondsSinceEpoch;
+                    final directory = await getApplicationDocumentsDirectory();
+                    String fileFormat = imageFile.path.split('.').last;
+
+                    await imageFile.copy(
+                       '${directory.path}/$currentUnix.$fileFormat',
+                    );
+
+                   */
+                  },
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: const [
+                      Icon(Icons.circle, color: Colors.white38, size: 80),
+                      Icon(Icons.circle, color: Colors.white, size: 65),
+                    ],
+                  ),
+                )
+                ,)
+
+        ],
+      ),
+    )]
+    )
     );
+
   }
 }
+
