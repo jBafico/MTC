8fb28314d6f152dfc2654d3b67e6057781f33950
Franco Bosetti <francobosetti@live.com.ar>
2022-12-01 02:36:33 -0300

    DEV: Delete movements

--- lib/Screens/Components/Cards/movement_card.dart
+++ lib/Screens/Components/Cards/movement_card.dart
@@ -1,11 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:maneja_tus_cuentas/Model/Movement.dart';
+import 'package:maneja_tus_cuentas/Services/database.dart';
 import 'package:maneja_tus_cuentas/constants.dart';
+
+import '../../../Model/UserData.dart';
+import '../../../Services/auth.dart';
 
 class MovementCard extends StatelessWidget {
   final Movement movement;
+  final DatabaseService _databaseService = DatabaseService(uid: AuthService().currentUser!.uid);
 
-  const MovementCard({Key? key, required this.movement}) : super(key: key);
+  MovementCard({Key? key, required this.movement}) : super(key: key);
+
+  Widget _buildPopupDialog(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Confirmar'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const <Widget>[
+          Text("¿Deseas eliminar este movimiento? Su balance se verá afectado. Esta acción no se puede deshacer."),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.red,
+            padding: const EdgeInsets.all(10.0),
+            textStyle: const TextStyle(fontSize: 15),
+          ),
+            child: const Text('ELIMINAR'),
+            onPressed: () async {
+              try {
+                _databaseService.removeMovement(movement);
+                // Get user data
+                UserData userData = await _databaseService.getUserData();
+
+                userData
+                    .updateBalance(movement.amount * (movement.type == 'income' ? -1 : 1));
+
+                _databaseService
+                    .updateUserBalance(userData.balance)
+                    .then((value) => Navigator.pop(context));
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Error al eliminar movimiento'),
+                  ),
+                );
+              }
+            }
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.all(10.0),
+            textStyle: const TextStyle(fontSize: 15),
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cerrar'),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +72,13 @@ class MovementCard extends StatelessWidget {
       shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(15))),
       elevation: 0,
+      child: InkWell(
+        onTap: () {
+          showDialog(
+              context: context,
+              builder: (BuildContext context) => _buildPopupDialog(context),
+          );
+        },
       child: Row(
         children: <Widget>[
           SizedBox(
@@ -60,6 +125,7 @@ class MovementCard extends StatelessWidget {
           ),
         ],
       ),
+      )
     );
   }
 }
