5586ee3eb309b4a09b5494dcb00cecaaaa7f7bf8
Franco Bosetti <francobosetti@live.com.ar>
2022-12-04 18:53:52 -0300

    DEV: Better deleting and editing movements

--- lib/Screens/Components/Cards/movement_card.dart
+++ lib/Screens/Components/Cards/movement_card.dart
@@ -8,62 +8,8 @@ import '../../../Services/auth.dart';
 
 class MovementCard extends StatelessWidget {
   final Movement movement;
-  final DatabaseService _databaseService = DatabaseService(uid: AuthService().currentUser!.uid);
 
-  MovementCard({Key? key, required this.movement}) : super(key: key);
-
-  Widget _buildPopupDialog(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Confirmar'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const <Widget>[
-          Text("¿Deseas eliminar este movimiento? Su balance se verá afectado. Esta acción no se puede deshacer."),
-        ],
-      ),
-      actions: <Widget>[
-        TextButton(
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.red,
-            padding: const EdgeInsets.all(10.0),
-            textStyle: const TextStyle(fontSize: 15),
-          ),
-            child: const Text('ELIMINAR'),
-            onPressed: () async {
-              try {
-                _databaseService.removeMovement(movement);
-                // Get user data
-                UserData userData = await _databaseService.getUserData();
-
-                userData
-                    .updateBalance(movement.amount * (movement.type == 'income' ? -1 : 1));
-
-                _databaseService
-                    .updateUserBalance(userData.balance)
-                    .then((value) => Navigator.pop(context));
-              } catch (e) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Error al eliminar movimiento'),
-                  ),
-                );
-              }
-            }
-        ),
-        TextButton(
-          style: TextButton.styleFrom(
-            padding: const EdgeInsets.all(10.0),
-            textStyle: const TextStyle(fontSize: 15),
-          ),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('Cerrar'),
-        ),
-      ],
-    );
-  }
+  const MovementCard({Key? key, required this.movement}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -73,12 +19,6 @@ class MovementCard extends StatelessWidget {
           borderRadius: BorderRadius.all(Radius.circular(15))),
       elevation: 0,
       child: InkWell(
-        onTap: () {
-          showDialog(
-              context: context,
-              builder: (BuildContext context) => _buildPopupDialog(context),
-          );
-        },
       child: Row(
         children: <Widget>[
           SizedBox(
