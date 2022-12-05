e565dc3635458bbdb8f02135b763a0a2ecfb3847
sranucci <sranucci@itba.edu.ar>
2022-12-05 11:59:08 -0300

    DEV: charts are now clickable

--- lib/Screens/Components/BarChart/net_worth_bar_chart.dart
+++ lib/Screens/Components/BarChart/net_worth_bar_chart.dart
@@ -8,32 +8,60 @@ import 'package:syncfusion_flutter_charts/charts.dart';
 
 import 'bar_chart_data.dart';
 
-class NetworthBarChart extends StatelessWidget{
+
+class NetworthBarChart extends StatefulWidget {
   final List<BarChartData> chartDataList;
   final String title;
-  const NetworthBarChart({Key? key,required this.chartDataList, required this.title}) : super(key: key);
 
-  MaterialColor getColor(String category){
-    return category == "Ingresos" ? Colors.green:Colors.red;
+  const NetworthBarChart(
+      {Key? key, required this.chartDataList, required this.title})
+      : super(key: key);
+
+  MaterialColor getColor(String category) {
+    return category == "Ingresos" ? Colors.green : Colors.red;
   }
+
+
+  @override
+  State<StatefulWidget> createState() => _BarChartState();
+}
+
+
+
+
+
+
+
+
+class _BarChartState extends State<NetworthBarChart> {
+
+
+  bool dataVisible = false;
+
 
   @override
   Widget build(BuildContext context) {
     return SfCartesianChart(
-        title: ChartTitle(text: title,textStyle: const TextStyle(decoration: TextDecoration.underline, fontWeight: FontWeight.bold, decorationColor: Colors.green)),
+        title: ChartTitle(text: widget.title,
+            textStyle: const TextStyle(decoration: TextDecoration.underline,
+                fontWeight: FontWeight.bold,
+                decorationColor: Colors.green)),
         primaryXAxis: CategoryAxis(),
         series: <ChartSeries>[
           ColumnSeries<BarChartData, dynamic>(
             //ancho de columna es inversamente proporcional al spacing
               spacing: 0.7,
-              dataSource: chartDataList,
-              xValueMapper: ( data, _) => data.category,
-              yValueMapper: ( data, _) => data.total,
-              pointColorMapper: ( data, _) => getColor(data.category),
+              dataSource: widget.chartDataList,
+              xValueMapper: (data, _) => data.category,
+              yValueMapper: (data, _) => data.total,
+              dataLabelSettings: DataLabelSettings(isVisible: dataVisible),
+              pointColorMapper: (data, _) => widget.getColor(data.category),
+              onPointTap: (e) => setState( () => dataVisible = !dataVisible),
               borderRadius: const BorderRadius.all(Radius.circular(15)
               )
           )
         ]
+
     );
   }
 }
